8702ccdfef8448bcb910a1394b5bc8625d9ba662
Andrew Barchuk <andrew@raindev.pp.ua>
2013-06-07 02:32:01 +0300

    Lab1 done.

--- src/ua/kpi/security/CardEventsLister.java
+++ src/ua/kpi/security/CardEventsLister.java
@@ -6,25 +6,46 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.sql.*;
-import java.util.ArrayList;
-import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
 /**
  * List card events by station hall name and card number.
  */
-@WebServlet(name = "CardEventsListener", urlPatterns="/lab1")
+@WebServlet(name = "CardEventsListener", urlPatterns = "/lab1")
 public class CardEventsLister extends HttpServlet {
-    private static final String DB_PATH = "/home/rain/Desktop/";
-    private static final String HALL_PARAM = "hall";
+    private static final String STATION_PARAM = "hall";
     private static final String CARD_PARAM = "card";
+    private static Properties properties;
+    private static Properties stationNames;
+    private static Properties errorCodes;
+    private static Properties eventCodes;
 
     static {
         try {
             Class.forName("org.sqlite.JDBC");
-        } catch (ClassNotFoundException ignored) {
-
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
         }
+        stationNames = new Properties();
+        errorCodes = new Properties();
+        eventCodes = new Properties();
+        properties = new Properties();
+        try {
+            stationNames.load(new InputStreamReader(CardEventsLister.class.getClassLoader().getResourceAsStream("/stationNames.properties")));
+            errorCodes.load(new InputStreamReader(CardEventsLister.class.getClassLoader().getResourceAsStream("/errors.properties")));
+            eventCodes.load(new InputStreamReader(CardEventsLister.class.getClassLoader().getResourceAsStream("/events.properties")));
+            properties.load(new InputStreamReader(CardEventsLister.class.getClassLoader().getResourceAsStream("/events.properties")));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -32,26 +53,44 @@ public class CardEventsLister extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String hall = request.getParameter(HALL_PARAM);
+        request.setCharacterEncoding("utf-8");
+        String station = request.getParameter(STATION_PARAM);
         String card = request.getParameter(CARD_PARAM);
-//        if (hall != null && card != null) {
-//            if (hall.equals("Чернігівська (схід)")) {
+        query:
+        {
+            if (station != null && card != null) {
                 try {
-                    Connection connection = DriverManager.getConnection("jdbc:sqlite:" + DB_PATH + "adbk.n0265.11.db3");
-                    PreparedStatement statement = connection.prepareStatement("SELECT  * FROM events WHERE cardNo = ? LIMIT 100");
+                    if (stationNames.get(station) == null) {
+                        log(station);
+                        log(stationNames.propertyNames().nextElement().toString());
+                        log(stationNames.get(stationNames.propertyNames().nextElement()).toString());
+                        request.setAttribute("error", true);
+                        break query;
+                    }
+                    Connection connection = DriverManager.getConnection("jdbc:sqlite:" + properties.get("db_path") + "adbk." + stationNames.get(station) + ".11.db3");
+                    PreparedStatement statement = connection.prepareStatement("SELECT DateTime date, ErrorCode error, EventCode event FROM events WHERE cardNo = ?");
                     statement.setString(1, card);
                     ResultSet result = statement.executeQuery();
-                    List<String> cardNumbers = new ArrayList<>();
+                    Map<Date, String> events = new HashMap<>();
                     while (result.next()) {
-                        log(result.getString("ErrorCode"));
-                        cardNumbers.add(result.getString("ErrorCode"));
+                        String error = result.getString("error");
+                        String message = error.equals("0") ?
+                                eventCodes.getProperty(result.getString("event")) :
+                                errorCodes.getProperty(error);
+                        events.put(new SimpleDateFormat("YYYYMMDDHHMMSS").parse(result.getString("date")),
+                                message != null ? message : "Невідома операція");
                     }
-                    System.out.println("Connected");
-                    request.getRequestDispatcher("lab1.jsp").forward(request, response);
+                    request.setAttribute("events", events);
+                    request.setAttribute("fillList", true);
                 } catch (SQLException e) {
                     throw new ServletException(e);
+                } catch (ParseException e) {
+                    e.printStackTrace();
                 }
-//            }
-//        }
+            } else {
+                request.setAttribute("fillList", false);
+            }
+        }
+        request.getRequestDispatcher("lab1.jsp").forward(request, response);
     }
 }
--- web/lab1.jsp
+++ web/lab1.jsp
@@ -1,9 +1,13 @@
+<%@ page import="java.util.Date" %>
+<%@ page import="java.util.Map" %>
 <%--
   List of events by station hall and
 --%>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<!doctype html>
 <html>
 <head>
+    <meta charset="utf-8">
     <title>Lab1</title>
 </head>
 <body>
@@ -18,5 +22,20 @@
         </label>
         <input type="submit">
     </form>
+    <table style="margin-left: 25px; margin-top: 10px; border: 1px solid black;">
+    <%
+        if (request.getAttribute("error") != null) {
+            %><h2>Cannot find station</h2><%
+        }else{
+        if ((Boolean) request.getAttribute("fillList")) {
+            @SuppressWarnings("unchecked")
+            Map<Date, String> events = (Map<Date, String>) request.getAttribute("events");
+            for (Map.Entry<Date, String> event : events.entrySet()) { %>
+                <tr><td style="border: 1px solid black;"><%= event.getKey()%></td><td style="border: 1px solid black;"><%= event.getValue()%></td></tr>
+            <%}
+        }
+            }
+    %>
+    </table>
 </body>
 </html>
